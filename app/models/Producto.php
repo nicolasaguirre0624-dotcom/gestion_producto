538140decbaca39759f4249d7480c123b5eb77f7
Erick nicolas <nicolasaguirre0624@gmail.com>
2026-09-07 17:35:55 -0500

    Se completa la relacion de productos con proveedores

--- app/models/Producto.php
+++ app/models/Producto.php
@@ -11,11 +11,11 @@ class Producto {
         $this->connection = $database->connect();
     }
 
-    public function getAll(){
-        $sql = "SELECT * FROM productos";
-
+   public function getAll(){
+        $sql = "SELECT productos.*, proveedor.nombre AS nombre_proveedor 
+                FROM productos 
+                LEFT JOIN proveedor ON productos.id_proveedor = proveedor.id";
         $consulta = $this->connection->query($sql);
-
         return $consulta->fetchAll(PDO::FETCH_ASSOC);
     }
 }
--- app/views/producto/index.php
+++ app/views/producto/index.php
@@ -4,12 +4,14 @@
         <th>Nombre</th>
         <th>Precio</th>
         <th>Stock</th>
+        <th>Proveedor</th>
     </tr>
     <?php foreach ($productos as $producto): ?>
     <tr>
         <td><?= $producto['nombre'] ?></td>
         <td><?= $producto['precio'] ?></td>
         <td><?= $producto['stock'] ?></td>
+      <td><?= $producto['nombre_proveedor'] ?></td>
     </tr>
     <?php endforeach; ?>
 </table>
