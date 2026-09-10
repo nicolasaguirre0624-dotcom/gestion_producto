453f8849568262e8b2c02f12038d9e819e024340
Erick nicolas <nicolasaguirre0624@gmail.com>
2026-09-10 15:36:10 -0500

    datos actualizados

--- app/models/Producto.php
+++ app/models/Producto.php
@@ -12,9 +12,9 @@ class Producto {
     }
 
    public function getAll(){
-        $sql = "SELECT productos.*, proveedor.nombre AS nombre_proveedor 
-                FROM productos 
-                LEFT JOIN proveedor ON productos.id_proveedor = proveedor.id";
+       $sql = "SELECT productos.*, proveedor.nombre AS nombre_proveedor
+        FROM productos
+        LEFT JOIN proveedor ON productos.id_proveedor = proveedor.id";
         $consulta = $this->connection->query($sql);
         return $consulta->fetchAll(PDO::FETCH_ASSOC);
     }
